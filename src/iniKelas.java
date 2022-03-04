27c34ba571eabd2af518b2144af61a64350b009d
auliamasadah04 <auliamasadah@gmail.com>
2022-03-04 08:24:29 +0700

    am

---
+++ src/iniKelas.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aulia
+ */
+public class iniKelas {
+    
+   String nama="Aulia";
+    String jk= "perempuan";
+    String alamat =" Malang";
+    
+    
+    public void tampilNama(){
+        System.out.println("Nama Siswa: "+nama);
+    }
+    
+    public void tampilJenisKelamin(){
+        System.out.println("Jenis Kelamin: "+jk);
+    }
+    
+    public void tampilAlamat(){
+        System.out.println("Alamat: "+alamat);
+    }
+    
+}
---
+++ src/wujudObjek.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aulia
+ */
+public class wujudObjek {
+    public static void main(String[] args) {
+        iniKelas ik=new iniKelas();
+        ik.tampilNama();
+        ik.tampilJenisKelamin();
+        ik.tampilAlamat();
+    }
+}
